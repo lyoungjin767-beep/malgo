53a54356ca80a2e9bbb758afc4a8e179bf933b5d
lyoungjin <emforhsjan@naver.com>
2026-08-04 20:48:51 +0900

    match member repository format

--- backend/src/main/java/com/malgo/backend/member/repository/MemberRepository.java
+++ backend/src/main/java/com/malgo/backend/member/repository/MemberRepository.java
@@ -1,8 +1,9 @@
 package com.malgo.backend.member.repository;
 
 import com.malgo.backend.member.entity.Member;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
