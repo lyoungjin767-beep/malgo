a85b9fd6430f6bfe37cda91d80b9f5020482a1fa
lyoungjin <emforhsjan@naver.com>
2026-08-04 20:22:39 +0900

    match member entity format

--- backend/src/main/java/com/example/backend/member/entity/Member.java
+++ backend/src/main/java/com/example/backend/member/entity/Member.java
@@ -1,15 +1,11 @@
 package com.example.backend.member.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
